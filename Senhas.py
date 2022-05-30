7b5ba3dca99e0e6b9c3ee50f82363f3a1ac8230b
Giovanni Schardong <100441428+gioschardong@users.noreply.github.com>
2022-05-30 10:35:07 -0300

    Update Senhas.py

--- Senhas.py
+++ Senhas.py
@@ -5,10 +5,11 @@ valores = string.ascii_letters  # todas as letras maiusculas e minusculas
 valores += string.digits  # Numeros de 0 a 9
 valores += string.punctuation  # Caracteres especiais
 
-tamanho = 8
+print('Insira o numero de caracteres da senha: ')
+tamanho = int(input())
 senha = ""
 
 for i in range(tamanho):
     senha += choice(valores)
 
-print(senha)
+print('Sua senha é: '+senha)
